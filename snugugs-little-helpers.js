7e556082ba59a21b41fd99e5fabaa9207741e7c7
Sam Richard <snugug@google.com>
2022-04-23 07:25:23 -0400

    Migrate to hooks-based condition handling

--- snugugs-little-helpers.js
+++ snugugs-little-helpers.js
@@ -1,10 +1,30 @@
-import { applyCondition } from './lib/cub/apply-condition.js';
+import { MODULE } from './lib/constants.js';
+import {
+  applyCondition,
+  conditionRegister,
+  toggleTieredCondition,
+  removeTieredCondition,
+  resetTieredCondition,
+} from './lib/cub/apply-condition.js';
 
 export class SnugugsLittleHelpers {
   static applyCondition = applyCondition;
+  static removeTieredCondition = removeTieredCondition;
+  static resetTieredCondition = resetTieredCondition;
 
   static async init() {
     game.SnugugsLittleHelpers = this;
+  }
+
+  static async ready() {
+    // Get registered conditions
+    if (game.user.isGM) {
+      const existingConditions = game.user?.getFlag(MODULE.ID, MODULE.FLAGS.CONDITIONS);
+
+      if (existingConditions) {
+        conditionRegister.set(existingConditions);
+      }
+    }
   }
 }
 
@@ -12,26 +32,32 @@ Hooks.on('init', () => {
   SnugugsLittleHelpers.init();
 });
 
-// Hooks.on('createActiveEffect', (args) => {
-//   if (args.data) {
-//     const isCondition = args.data?.flags?.['combat-utility-belt']?.conditionId;
+Hooks.on('ready', () => {
+  SnugugsLittleHelpers.ready();
+});
 
-//     if (isCondition) {
-//       const condition = args.data.label;
-//       console.log('Adding ' + condition);
-//     } else {
-//       console.log('Not a condition');
-//     }
-//   }
-// });
+Hooks.on('createActiveEffect', (args) => {
+  if (args.data) {
+    const isCondition = args.data?.flags?.['combat-utility-belt']?.conditionId;
 
-// Hooks.on('deleteActiveEffect', (args) => {
-//   const isCondition = args.data?.flags?.['combat-utility-belt']?.conditionId;
+    if (isCondition) {
+      const condition = args.data.label;
+      console.log('Adding ' + condition);
+      toggleTieredCondition(condition, true);
+    } else {
+      console.log('Not a condition');
+    }
+  }
+});
 
-//   if (isCondition) {
-//     const condition = args.data.label;
-//     console.log('Removing ' + condition);
-//   } else {
-//     console.log('Not a condition');
-//   }
-// });
+Hooks.on('deleteActiveEffect', (args) => {
+  const isCondition = args.data?.flags?.['combat-utility-belt']?.conditionId;
+
+  if (isCondition) {
+    const condition = args.data.label;
+    console.log('Removing ' + condition);
+    toggleTieredCondition(condition, false);
+  } else {
+    console.log('Not a condition');
+  }
+});
